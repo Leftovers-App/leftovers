7dd7b8bf2b0e3125edc240d2a7a1b6490f45ec16
Jonathan <warkentin.jon@gmail.com>
2019-10-26 17:23:48 -0500

    make maps work

--- components/TransportScreen.js
+++ components/TransportScreen.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 import {
     Platform,
     StyleSheet,
@@ -7,6 +8,19 @@ import {
     Button
 } from 'react-native';
 
+const styles = StyleSheet.create({
+    container: {
+        ...StyleSheet.absoluteFillObject,
+        height: 400,
+        width: 400,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+    },
+    map: {
+        ...StyleSheet.absoluteFillObject,
+    },
+});
+
 class TransportScreen extends React.Component {
     static navigationOptions = {
         title: 'Transport',
@@ -14,8 +28,18 @@ class TransportScreen extends React.Component {
     render() {
         const { navigate } = this.props.navigation;
         return (
-            <View>
-                <Text>This is the Transport screen.</Text>
+            <View style={styles.container}>
+                <MapView
+                    provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+                    style={styles.map}
+                    region={{
+                        latitude: 37.78825,
+                        longitude: -122.4324,
+                        latitudeDelta: 0.015,
+                        longitudeDelta: 0.0121,
+                    }}
+                >
+                </MapView>
             </View>
         );
     }
