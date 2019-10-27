4976d8ab2ed28d525605c9017b8a5e66388535d0
Jonathan <warkentin.jon@gmail.com>
2019-10-27 02:19:04 -0500

    resolve merge conflict

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -5,7 +5,6 @@ import {
     View,
     PermissionsAndroid
 } from 'react-native';
-import Geolocation from '@react-native-community/geolocation';
 
 
 
@@ -15,75 +14,21 @@ class HomeScreen extends React.Component {
     static navigationOptions = {
         title: 'LEFTOVERS',
     };
-
-    async componentDidMount() {
-        try {
-            const granted1 = await PermissionsAndroid.request(
-                PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-                {
-                    title: 'LEFTOVERS Location Permission',
-                    message:
-                        'LEFTOVERS needs access to your location ' +
-                        'to connect you to other users..',
-                    buttonNeutral: 'Ask Me Later',
-                    buttonNegative: 'Cancel',
-                    buttonPositive: 'OK',
-                }
-            );
-            const granted2 = await PermissionsAndroid.request(
-                PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION,
-                {
-                    title: 'LEFTOVERS Location Coarse Permission',
-                    message:
-                        'LEFTOVERS needs access to your coarse location ' +
-                        'to connect you to other users..',
-                    buttonNeutral: 'Ask Me Later',
-                    buttonNegative: 'Cancel',
-                    buttonPositive: 'OK',
-                }
-            );
-            if ((granted1 === PermissionsAndroid.RESULTS.GRANTED) && (granted2 === PermissionsAndroid.RESULTS.GRANTED)) {
-                console.log("permission granted for fine location!");
-                // const realLocation = () => {
-                Geolocation.getCurrentPosition(position => {
-                    let lat = parseFloat(position.coords.latitude)
-                    let long = parseFloat(position.coords.longitude)
-                    console.log("inside getCurrentPosition");
-                    const initialRegion = {
-                        latitude: lat,
-                        longitude: long,
-                        latitudeDelta: 1,
-                        longitudeDelta: 1
-                    }
-                    console.log(initialRegion);
-                },
-                    (error) => alert(JSON.stringify(error)),
-                    { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 });
-                // }
-                // console.log(realLocation);
-            } else {
-                console.log('Fine Location permission denied');
-            }
-        } catch (err) {
-            console.warn(err);
-        }
-    }
-
     render() {
         const { navigate } = this.props.navigation;
         return (
             <View>
                 <Text></Text>
-                <Button bordered style={{alignSelf:'center'}} onPress={() => navigate('Sender')}>
-                    <Text style={{color: 'blue'}}>Sender</Text>
-                 </Button>
-                 <Text></Text>
-                 <Button bordered style={{alignSelf:'center'}} onPress={() => navigate('Receiver')}>
-                    <Text style={{color: 'blue'}}>Receiver</Text>
+                <Button bordered style={{ alignSelf: 'center' }} onPress={() => navigate('Sender')}>
+                    <Text style={{ color: 'blue' }}>Sender</Text>
                 </Button>
                 <Text></Text>
-                <Button bordered style={{alignSelf:'center'}} onPress={() => navigate('Transport')}>
-                    <Text style={{color: 'blue'}}>Transport</Text>
+                <Button bordered style={{ alignSelf: 'center' }} onPress={() => navigate('Receiver')}>
+                    <Text style={{ color: 'blue' }}>Receiver</Text>
+                </Button>
+                <Text></Text>
+                <Button bordered style={{ alignSelf: 'center' }} onPress={() => navigate('Transport')}>
+                    <Text style={{ color: 'blue' }}>Transport</Text>
                 </Button>
             </View>
         );
--- components/TransportScreen.js
+++ components/TransportScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
 import {
     Platform,
     StyleSheet,
@@ -10,7 +10,6 @@ import {
     PermissionsAndroid
 } from 'react-native';
 import ActionButton from 'react-native-action-button';
-import Geolocation from '@react-native-community/geolocation';
 
 const styles = StyleSheet.create({
     container: {
@@ -32,35 +31,16 @@ class TransportScreen extends React.Component {
         super()
         this.state = {
             initialPosition: {
-                latitude: .65,
-                longitude: .65,
-                latitudeDelta: 1,
-                longitudeDelta: 1
+                latitude: 41.883885,
+                longitude: -87.653632,
+                latitudeDelta: .02,
+                longitudeDelta: .02
             }
         }
     }
 
     async componentDidMount() {
         const { navigate } = this.props.navigation;
-        // const realLocation = () => {
-        //     Geolocation.getCurrentPosition(position => {
-        //         let lat = parseFloat(position.coords.latitude)
-        //         let long = parseFloat(position.coords.longitude)
-
-        //         const initialRegion = {
-        //             latitude: lat,
-        //             longitude: long,
-        //             latitudeDelta: 1,
-        //             longitudeDelta: 1
-        //         }
-        //         console.log(initialRegion);
-        //         return initialRegion;
-        //     },
-        //         (error) => alert(JSON.stringify(error)),
-        //         { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 });
-        // }
-        // console.log(realLocation);
-        // this.setState({ initialPosition: realLocation });
     }
 
 
@@ -73,6 +53,7 @@ class TransportScreen extends React.Component {
                     style={styles.map}
                     region={this.state.initialPosition}
                 >
+                    <Marker coordinate={{ latitude: 41.883885, longitude: -87.653632 }} />
                 </MapView>
                 <ActionButton buttonColor="rgba(231,76,60,1)" position="right" onPress={() => navigate('Transport2')}>
                 </ActionButton>
