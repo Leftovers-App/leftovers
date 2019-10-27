c84ab73dbba83f74a5953d99235501ac5f39099b
Sean Higgins <shiggins@luc.edu>
2019-10-26 21:39:51 -0500

    added dummy data and update to reciever screen

--- components/ReceiverScreen.js
+++ components/ReceiverScreen.js
@@ -4,25 +4,82 @@ import {
     StyleSheet,
     Text,
     View,
-    Button
+    Button,
+    ScrollView,
 } from 'react-native';
 
-class ReceiverScreen extends React.Component {
-    static navigationOptions = {
-        title: 'Receiver',
-    };
-    render() {
-        const { navigate } = this.props.navigation;
+import {
+  // Header,
+  // LearnMoreLinks,
+  Colors,
+  // DebugInstructions,
+  // ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+var dummy_data = require('../DummyData/listItem.json');
+const jobs = dummy_data.jobs
+
+
+class HeaderLogo extends React.Component {
+    render () {
         return (
-            <View>
-                <Text>This is the Receiver screen.</Text>
-                <Button
-                    title="Receiver Second"
-                    onPress={() => navigate('Receiver2')}
-                />
-            </View>
+            <Text>Reciever2</Text>
         );
     }
 }
+
+class ReceiverScreen extends React.Component {
+    static navigationOptions = ({navigation}) => {
+        return {
+            headerTitle: () => <HeaderLogo />,
+            headerRight: () => (
+                <Button
+                    title="+"
+                    onPress={() => navigation.navigate('Receiver2')}
+                />
+            ),
+        };
+    };
+    constructor() {
+        super();
+
+        this.state = {
+            jobs: []
+        };
+    }
+
+    render() {
+        const { navigate } = this.props.navigation;
+
+        return (
+            <ScrollView
+            contentInsetAdjustmentBehavior="automatic"
+            style={styles.scrollView}>
+                <View>
+                    <Text>Current</Text>
+                    <Button
+                        title="Receiver Second"
+                        onPress={() => navigate('Receiver2')}
+                    />
+                </View>
+                <View>
+                    <Text>All Available</Text>
+                    <Button
+                        title={jobs[0].name}
+                        onPress={() => navigate('Receiver2')}  //send all of jobs through to next page?
+                    />
+                    <Text> {jobs[0].name} </Text>
+                    <Text> {jobs[0].availability} </Text>
+                </View>
+            </ScrollView>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+});
 
 export default ReceiverScreen;
