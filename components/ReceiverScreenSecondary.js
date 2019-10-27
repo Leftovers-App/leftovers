1b78889008cbba603732d51dcfe3a6d43757bdaa
Martin Zugschwert <martin.e.zugschwert@gmail.com>
2019-10-26 21:20:51 -0500

    Adding a picture and more stuff of that sort on the receiver second page

--- components/ReceiverScreenSecondary.js
+++ components/ReceiverScreenSecondary.js
@@ -4,7 +4,8 @@ import {
     StyleSheet,
     Text,
     View,
-    Button
+    Button,
+    Image
 } from 'react-native';
 
 class ReceiverScreenSecondary extends React.Component {
@@ -16,6 +17,18 @@ class ReceiverScreenSecondary extends React.Component {
         return (
             <View>
                 <Text>This is the Secondary Receiver screen.</Text>
+                <Image 
+                    source={require('./hackathon.jpg')}  
+                    style={{width: 400, height: 400}} 
+                />
+                <Text>This is extra food from an amazing Dare Mighty Things hackathon!</Text>
+                <Text>Sender Information</Text>
+                <Text>Name: </Text>
+                <Text>Address: </Text>
+                <Button
+                    title="Request pickup"
+                    onPress={() => navigate('Receiver')}
+                />
             </View>
         );
     }
