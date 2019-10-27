33d06a34cd0ac79970dc420b26ead754da34bb34
Jonathan <warkentin.jon@gmail.com>
2019-10-26 22:43:00 -0500

    resolve merge conflict again

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -4,13 +4,71 @@ import {
     StyleSheet,
     Text,
     View,
-    Button
+    Button,
+    PermissionsAndroid
 } from 'react-native';
+import Geolocation from '@react-native-community/geolocation';
+
+
 
 class HomeScreen extends React.Component {
     static navigationOptions = {
         title: 'LEFTOVERS',
     };
+
+    async componentDidMount() {
+        try {
+            const granted1 = await PermissionsAndroid.request(
+                PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+                {
+                    title: 'LEFTOVERS Location Permission',
+                    message:
+                        'LEFTOVERS needs access to your location ' +
+                        'to connect you to other users..',
+                    buttonNeutral: 'Ask Me Later',
+                    buttonNegative: 'Cancel',
+                    buttonPositive: 'OK',
+                }
+            );
+            const granted2 = await PermissionsAndroid.request(
+                PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION,
+                {
+                    title: 'LEFTOVERS Location Coarse Permission',
+                    message:
+                        'LEFTOVERS needs access to your coarse location ' +
+                        'to connect you to other users..',
+                    buttonNeutral: 'Ask Me Later',
+                    buttonNegative: 'Cancel',
+                    buttonPositive: 'OK',
+                }
+            );
+            if ((granted1 === PermissionsAndroid.RESULTS.GRANTED) && (granted2 === PermissionsAndroid.RESULTS.GRANTED)) {
+                console.log("permission granted for fine location!");
+                // const realLocation = () => {
+                Geolocation.getCurrentPosition(position => {
+                    let lat = parseFloat(position.coords.latitude)
+                    let long = parseFloat(position.coords.longitude)
+                    console.log("inside getCurrentPosition");
+                    const initialRegion = {
+                        latitude: lat,
+                        longitude: long,
+                        latitudeDelta: 1,
+                        longitudeDelta: 1
+                    }
+                    console.log(initialRegion);
+                },
+                    (error) => alert(JSON.stringify(error)),
+                    { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 });
+                // }
+                // console.log(realLocation);
+            } else {
+                console.log('Fine Location permission denied');
+            }
+        } catch (err) {
+            console.warn(err);
+        }
+    }
+
     render() {
         const { navigate } = this.props.navigation;
         return (
