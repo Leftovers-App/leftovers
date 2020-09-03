ddc9f2135fa2b99f3d2878df670b456de1ea0e8e
Jonathan <warkentin.jon@gmail.com>
2020-09-03 08:43:58 -0500

    add PostActionButtons componenet

---
+++ components/PostActionButtons.jsx
@@ -0,0 +1,49 @@
+import React as * from "react";
+
+export const AcceptJobButton = () => {
+    return (
+        null
+    );
+}
+
+export const CancelClaimButton = () => {
+    return (
+        null
+    );
+}
+
+export const CancelJobButton = () => {
+    return (
+        null
+    );
+}
+
+export const CancelOfferButton = () => {
+    return (
+        null
+    );
+}
+
+export const ClaimButton = () => {
+    return (
+        null
+    );
+}
+
+export const ConfirmDeliveryButton = () => {
+    return (
+        null
+    );
+}
+
+export const ConfirmPickupButton = () => {
+    return (
+        null
+    );
+}
+
+export const DenyJobButton = () => {
+    return (
+        null
+    );
+}
