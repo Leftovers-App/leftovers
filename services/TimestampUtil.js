901a1526e718eb23912de908d29c930f48e9dba3
Jonathan <warkentin.jon@gmail.com>
2020-10-24 02:56:43 -0500

    fix timestamp format

--- services/TimestampUtil.js
+++ services/TimestampUtil.js
@@ -4,7 +4,7 @@ const convertTimestamps = (postData) => {
     if (!postData) return null;
     let updatedData = postData;
     for (let timestamp of allTimestamps) {
-        if ((timestamp in postData) && postData[timestamp]) { updatedData[timestamp] = new Date(postData[timestamp].seconds * 1000).toString() }
+        if ((timestamp in postData) && postData[timestamp]) { updatedData[timestamp] = new Date(postData[timestamp].seconds * 1000).toLocaleString() }
     }
     return updatedData;
 }
